1618a12fd282b76bbb33e70f7b74af7886230145
bykbyk0401 <bykbyk0401@gmail.com>
2025-11-09 22:04:15 +0900

    setting: 프로젝트 생성

---
+++ linkid/src/main/java/com/example/linkid/LinkidApplication.java
@@ -0,0 +1,13 @@
+package com.example.linkid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkidApplication.class, args);
+	}
+
+}
---
+++ linkid/src/test/java/com/example/linkid/LinkidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.linkid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
